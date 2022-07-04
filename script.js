b00f531b2409aac820f93336d4d92d4a41b4d7f5
Matheus Campos de Oliveira <matheuswe2010@hotmail.com>
2022-07-04 17:49:29 -0300

    requisito 5

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-function popularQuadro (size){
-    const pai = document.getElementById('pixel-board')
-    for (let i = 0; i < size * size; i += 1){
-        let quadros = document.createElement('div')
-        quadros.classList.add('pixel')
-        quadros.style.backgroundColor = 'white'
-        pai.appendChild(quadros)
-    }
+function popularQuadro(size) {
+  const pai = document.getElementById('pixel-board');
+  for (let i = 0; i < size; i += 1) {
+    let quadros = document.createElement('div');
+    quadros.classList.add('pixel');
+    quadros.style.backgroundColor = 'white';
+    pai.appendChild(quadros);
+  }
 }
-popularQuadro(5)
+popularQuadro(25);
