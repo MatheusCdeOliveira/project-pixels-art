b6487cfaed4f9620c290e55faa218134c0b1fad4
Matheus Campos de Oliveira <matheuswe2010@hotmail.com>
2022-07-05 20:45:22 -0300

    requisito 12

--- script.js
+++ script.js
@@ -94,5 +94,34 @@ limpaTudo();
 function carregaPagina() {
   const corPreta = document.querySelector('.one');
   corPreta.classList.add('selected');
+
+  const colorTwo = document.querySelector('.two');
+  const colorThree = document.querySelector('.three');
+  const colorFour = document.querySelector('.four');
+
+  colorTwo.style.backgroundColor =
+    'rgb(' +
+    Math.floor(Math.random() * 256) +
+    ',' +
+    Math.floor(Math.random() * 256) +
+    ',' +
+    Math.floor(Math.random() * 256) +
+    ')';
+  colorThree.style.backgroundColor =
+    'rgb(' +
+    Math.floor(Math.random() * 256) +
+    ',' +
+    Math.floor(Math.random() * 256) +
+    ',' +
+    Math.floor(Math.random() * 256) +
+    ')';
+  colorFour.style.backgroundColor =
+    'rgb(' +
+    Math.floor(Math.random() * 256) +
+    ',' +
+    Math.floor(Math.random() * 256) +
+    ',' +
+    Math.floor(Math.random() * 256) +
+    ')';
 }
 window.onload = carregaPagina;
